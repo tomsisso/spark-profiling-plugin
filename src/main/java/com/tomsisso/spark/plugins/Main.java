ba4d78ade1da62df863e8572789b823ad22d4185
tomsisso <tomsisso@gmail.com>
2023-08-27 18:00:04 +0300

    refactor

--- src/main/java/com/tomsisso/spark/plugins/Main.java
+++ src/main/java/com/tomsisso/spark/plugins/Main.java
@@ -2,6 +2,6 @@ package com.tomsisso.spark.plugins;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
